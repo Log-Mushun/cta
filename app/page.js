e2471b27d1c209a4e4eceb29c4a05c01c72a61f5
Michelle <cadavidcarmona1@gmail.com>
2023-09-24 17:40:09 -0500

    BurgerMenu

--- app/page.js
+++ app/page.js
@@ -2,11 +2,15 @@ import WebGiViewer from "@/components/WebGiViewer"
 import Airports from "@/components/Airports"
 import DriverOne from "@/components/DriverOne"
 import LogoDarkComp from "@/components/LogoDarkComp"
+import BurgerMenu from "@/components/BurgerMenu"
 
 export default function Home() {
   return (
     <> 
-      <LogoDarkComp/>
+      <div className="flex flex-row">
+        <LogoDarkComp/>
+        <BurgerMenu/>
+      </div>
       <Airports/>
       <DriverOne/>
       <WebGiViewer/>
---
+++ components/BurgerMenu.jsx
@@ -0,0 +1,11 @@
+import {HiBars3} from 'react-icons-hi2'
+
+const BurgerMenu = () => {
+    return(
+        <div className="fixed right-10 pt-10">
+            Menu
+        </div>
+    )
+}
+
+export default BurgerMenu;
