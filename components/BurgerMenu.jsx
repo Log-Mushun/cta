b02074adcd6ed8c6e6467c82e3fa8f46904df81b
Michelle <cadavidcarmona1@gmail.com>
2023-09-24 17:40:47 -0500

    burguer menu

--- components/BurgerMenu.jsx
+++ components/BurgerMenu.jsx
@@ -1,9 +1,9 @@
-import {HiBars3} from 'react-icons-hi2'
+import {HiBars3} from 'react-icons/hi2'
 
 const BurgerMenu = () => {
     return(
         <div className="fixed right-10 pt-10">
-            Menu
+            <HiBars3/>
         </div>
     )
 }
