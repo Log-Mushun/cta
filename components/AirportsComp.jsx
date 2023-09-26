dd0508214ab6901cde6c68c32d79d3d9be0a297e
Mauricio Henao <atomauro@cosmos-2.local>
2023-09-26 15:55:49 -0500

    traducciones agregadas

--- components/AirportsComp.jsx
+++ components/AirportsComp.jsx
@@ -1,13 +1,27 @@
+"use client";
+import { I18nextProvider, useTranslation } from "react-i18next";
+import {i18n as i18nfile} from "../i18n";
+
 import AirportsFooter from "./AirportsFooter";
 
 const AirportsComp = () => {
+
+    const { t, i18n } = useTranslation();
+
+  /*   const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+    };
+ */
+
     return (
+        <I18nextProvider i18n={i18nfile}>
         <div className="bg-gradient-radial-a h-[100vh] flex flex-col justify-between">
             <div className="text-center pt-[4rem]">
-                <h1 className="text-7xl">AEROPUERTOS</h1>
+                <h1 className="text-7xl">{t('header-airport')}</h1>
             </div>
             <AirportsFooter/>
         </div>
+        </I18nextProvider>
     );
 };
 
---
+++ i18n.js
@@ -0,0 +1,31 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+const resources = {
+  en: {
+    translation: {
+      "header-home": "Home",
+      "header-airport": "Airport",
+    }
+  },
+  sp: {
+    translation: {
+      "header-home": "Inicio",
+      "header-airport": "Aeropuerto",
+    }
+  } 
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'sp',
+    fallbackLng: 'sp',
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
+
+export default i18n;
