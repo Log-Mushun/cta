121bf7fd044a0571ee9d513cfb45a74cf811475c
Mauricio Henao <atomauro@cosmos-2.local>
2023-09-26 18:12:28 -0500

    .

--- app/page.js
+++ app/page.js
@@ -61,11 +61,11 @@ const chars = ourText.chars
             {/* Título principal */}
               <h1
     id='titulo_selector'
-    className='text-3xl font-bold text-[#003d71] text-center mb-6 hover:opacity-20 transition-opacity duration-300 z-10'
+    className='text-3xl  font-bold text-[#003d71] text-center mb-6 hover:opacity-20 transition-opacity duration-300 z-10'
   >
     Selecciona tu experiencia
   </h1>
-            <div className='text-center flex flex-col items-center justify-center lg:gap-20 lg:pt-20  container mx-auto lg:flex-row'>
+            <div className='text-center flex flex-col items-center justify-center sm:gap-0 lg:gap-20 lg:pt-20  container mx-auto lg:flex-row'>
               {/* suitcase */}
               <div className='z-10 flex items-center flex-col mb-8'>
                 <Link href='/airports'>
@@ -77,7 +77,7 @@ const chars = ourText.chars
               </div>
               {/* papers */}
               <div style={{ userDrag: 'none' }} className='z-10 flex items-center flex-col mb-8'>
-                <Link style={{ userDrag: 'none' }} style={{ userDrag: 'none' }} href='/airports'>
+                <Link style={{ userDrag: 'none' }}  href='/airports'>
                   <div   className='bg-transparent hover:bg-hoverbg hover:bg-cover hover:bg-no-repeat h-[12.5rem] w-[12rem] flex justify-end flex-col items-center'>
                     <GiCardboardBoxClosed size={128}  className='text-[#003d71] lg:w-36 lg:h-36 md:w-20 md:h-20 ' />
                     <h1 className='text-[#003d71] text-center pt-5 text-2xl'>Papel y carton</h1>
