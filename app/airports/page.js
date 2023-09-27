29cd4e0841284b33d27b7034c178328d4fd774cc
Mauricio Henao <atomauro@cosmos-2.local>
2023-09-26 23:58:05 -0500

    .

--- app/airports/page.js
+++ app/airports/page.js
@@ -1,14 +1,14 @@
 import AirportsComp from "@/components/AirportsComp";
 import BurgerMenu from "@/components/BurgerMenu";
 import DriverOne from "@/components/DriverOne";
-import HeaderExperience from "@/components/Headers/HeaderExperience";
+import HeaderGeneral from "@/components/Headers/HeaderGeneral/HeaderGeneral";
 import WebGiViewer from "@/components/WebGiViewer";
 
 export default function Airports() {
   return (
     <> 
     <WebGiViewer/>
-     <HeaderExperience/>
+     <HeaderGeneral/>
       <AirportsComp/>
       < DriverOne/>
     </>
