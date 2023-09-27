e81cfd7bd554f992bba68df0b3646a8f016a652c
Michelle <cadavidcarmona1@gmail.com>
2023-09-26 21:59:59 -0500

    precarga

--- components/Plexus.jsx
+++ components/Plexus.jsx
@@ -7,7 +7,29 @@ const Plexus = () => {
 
   const canvasRef = useRef(null);
 
+  const preloadImages = () => {
+    for (let i = 1; i <= totalImages; i++) {
+      const img = new Image();
+      img.src = `/plexus/PMATEC_${i}.png`;
+    }
+  };
+
   useEffect(() => {
+    preloadImages();
+  }, []);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    const img = new Image();
+    img.src = `/plexus/PMATEC_${currentImage}.png`;
+
+    img.onload = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+
     const interval = setInterval(() => {
       setCurrentImage((prevImage) => {
         if (prevImage === totalImages) {
@@ -21,28 +43,16 @@ const Plexus = () => {
         }
       });
     }, 1000 / 30);
+
     return () => clearInterval(interval);
-  }, [totalImages, increment]);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-
-    const img = new Image();
-    img.src = `/plexus/PMATEC_${currentImage}.png`;
-
-    img.onload = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    };
-  }, [currentImage]);
+  }, [currentImage, increment, totalImages]);
 
   return (
     <canvas
       ref={canvasRef}
       className='w-full h-full z-0 mix-blend-hard-light'
-      width={1920}
-      height={1080}
+      width={1920} // Tamaño fijo del canvas
+      height={1080} // Tamaño fijo del canvas
     />
   );
 };
