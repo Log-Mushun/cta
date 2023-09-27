dae6275b82a2ef7c9a2638da8e6a0d60d4c2c34d
Mauricio Henao <atomauro@cosmos-2.local>
2023-09-26 22:16:29 -0500

    .

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,7 @@ import './globals.css'
 
 export const metadata = {
   title: 'MATEC',
-<<<<<<< HEAD
   description: 'MATEC Solutions',
-=======
-  description: 'MATEC',
->>>>>>> e81cfd7bd554f992bba68df0b3646a8f016a652c
 }
 
 export default function RootLayout({ children }) {
