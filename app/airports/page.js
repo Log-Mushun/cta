22bcadb3e031a3815f139496759d62664265b5c1
Michelle <cadavidcarmona1@gmail.com>
2023-10-02 13:55:40 -0500

    automatic scroll Driver One

--- app/airports/page.js
+++ app/airports/page.js
@@ -15,7 +15,11 @@ import { MyContextProvider } from "@/MyContextProvider";
 
 import React, { useState, useEffect } from "react";
 import GuardianConcept from "@/components/Experiences/airports/GuardianConcept";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 export default function Airports() {
   const [isWebGiViewerLoaded, setIsWebGiViewerLoaded] = useState(false);
@@ -26,33 +30,78 @@ export default function Airports() {
       document.body.style.overflowY = 'auto';
     };
 
-    document.addEventListener('webGiViewerLoaded', handleWebGiViewerLoaded);
 
-    return () => {
-      document.removeEventListener('webGiViewerLoaded', handleWebGiViewerLoaded);
-    };
-  }, []);
-  return (
+    const driverOne = document.getElementById('driver-one');
+    ScrollTrigger.create({
+      trigger: driverOne,
+      start: "top bottom",
+      end: "+=199%",
+      onToggle: self => self.isActive && !scrollTween && goToSectionOne()
+    });
+
+
+    let panels = gsap.utils.toArray(".panel"),
+      observer = ScrollTrigger.normalizeScroll(true),
+      scrollTween;
+
+    // on touch devices, ignore touchstart events if there's an in-progress tween so that touch-scrolling doesn't interrupt and make it wonky
+    document.addEventListener("touchstart", e => {
+      if (scrollTween) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+      }
+    }, { capture: true, passive: false })
+
+    function goToSectionOne() {
+      scrollTween = gsap.to(window, {
+        scrollTo: { y: innerHeight, autoKill: false },
+        onStart: () => {
+          document.body.style.overflow = 'hidden';
+          observer.disable(); // for touch devices, as soon as we start forcing scroll it should stop any current touch-scrolling, so we just disable() and enable() the normalizeScroll observer
+          observer.enable();
+          console.log('start');
+        },
+        duration: 1,
+        onComplete: () => {scrollTween = null; },
+        overwrite: true
+      });
+    }
+
+  document.addEventListener('webGiViewerLoaded', handleWebGiViewerLoaded);
+
+  return () => {
+    document.removeEventListener('webGiViewerLoaded', handleWebGiViewerLoaded);
+  };
+}, []);
+return (
   <MyContextProvider>
-      <LoadingScreen isVisible={!isWebGiViewerLoaded}/>
-      <WebGiViewer />
-      <Plexus isStart={false}/>
-      <HeaderGeneral/>
-      <HeaderExperience />
+    <LoadingScreen isVisible={!isWebGiViewerLoaded} />
+    <WebGiViewer />
+    <Plexus isStart={false} />
+    <HeaderGeneral />
+    <HeaderExperience />
+    <section className="panel h-full">
       <AirportsComp />
+    </section>
+    <section className="panel h-full">
       <div className="relative w-screen mr-0 ml-0">
         <DriverOne />
         <PlusButton number={1} />
         <SlidableDriverInfo number={1} />
       </div>
-      {/*SlidableDriverInfo affects the layout of the next element, so we have to let an empty div here in order to avoid the bug */}
-      <div>
-      </div>
+    </section>
+    {/*SlidableDriverInfo affects the layout of the next element, so we have to let an empty div here in order to avoid the bug */}
+    <div>
+    </div>
+    <section className="panel h-full">
       <div className="relative w-screen mr-0 ml-0">
         <DriverInsights />
       </div>
+    </section>
+    <section className="panel h-full">
       <DriverOneFunctions />
-      <GuardianConcept/>
-    </MyContextProvider>
-  )
+    </section>
+    <GuardianConcept />
+  </MyContextProvider>
+)
 }
