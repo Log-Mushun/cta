e706243e05ffc99d39828efea9d251f680826568
Mauricio Henao <atomauro@cosmos-2.local>
2023-10-02 01:44:07 -0500

    hola

--- components/Experiences/airports/DriverOne.jsx
+++ components/Experiences/airports/DriverOne.jsx
@@ -3,7 +3,7 @@ import PlusButton from "../../PlusButton";
 
 const DriverOne = () => {
     return(
-        <div className="bg-transparent z-[11] h-[100vh] text-right pr-40 pb-30 w-[100%] left-0 relative" id="driver-one">
+        <div className="bg-gradient-radial-b  z-[1] h-[100vh] text-right pr-40 pb-30 w-[100%] left-0 relative" id="driver-one">
             <h1 className="text-6xl relative right-[25%] top-[10%] opacity-0 driver-title z-[11]">
                 Driver #1
             </h1>
